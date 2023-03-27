1555f926a22c13eae1b100647dd025cadab20b05
Ben-Zard <you@example.com>
2023-03-26 21:30:08 -0400

    start login modal

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {useState} from 'react'
+import Login from './Login'
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+  const [show, setShow] = useState(false)
+  
   return (
-    <div>Navbar</div>
+    <div>
+      <button onClick={()=>setShow(!show)}> Login </button>
+      {show && <div> <Login/> </div>}
+
+    </div>
   )
 }
 
 export default Navbar
+
